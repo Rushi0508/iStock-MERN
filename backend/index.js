846c9cbd21348111a626fa5973deb2275ebfa0f1
Rushi Gandhi <88783422+Rushi0508@users.noreply.github.com>
2023-05-12 23:47:08 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -18,8 +18,7 @@ mongoose.connect(dbURL, {useNewUrlParser: true}).then(()=>{
 app.use(cookieParser());
 app.use(cors(
     {
-        origin: ["https://i-stock.vercel.app",
-                "http://localhost:3000"],
+        origin: ["https://i-stock.vercel.app"],
         methods: ["GET", "POST"],
         credentials: true,
     }
